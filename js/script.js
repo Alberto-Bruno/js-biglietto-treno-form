77c71353489ed445b52787c25cdca3a53380f670
Alberto-Bruno <brunoalberto97@outlook.it>
2022-12-06 17:32:47 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+console.log('JS OK')
+
+/*
+-- Chiediamo il nome all'utente
+1- Il prezzo del biglietto è definito in base ai km (0.21 € al km)
+2- Va applicato uno sconto del 20% per i minonenni
+3- Va applicato una sconto del 40% per gli over 65
+4- L'output del prezzo finale va messo fuori in forma umana
+*/
+
+
+// Prendo l'elemento dalla pagina
+const input = document.getElementById('kms')
+
+
+//Main Logics
+
+// 1- Il prezzo del biglietto definito in base ai km
+
+// Problemi
+if (isNaN (kms) || isNaN(age) || kms <= 0 || age <= 0) {
+    document.getElementById('alert').innerText = 'Caratteri non validi';
+} else {
+
+}
+
